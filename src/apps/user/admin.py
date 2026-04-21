0ed97c29f8f84e51a169460dbe524f8012f1a3f5
iskandar7171 <tolagankhojaeviskandar@gmail.com>
2026-04-21 16:16:06 +0300

    add: user models and admin

--- src/apps/user/admin.py
+++ src/apps/user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Seller)
+admin.site.register(SellerWallet)
+admin.site.register(Client)
+admin.site.register(Cart)
+admin.site.register(Favorite)
+admin.site.register(Tag)
+admin.site.register(LeadStatus)
--- src/config/settings.py
+++ src/config/settings.py
@@ -102,7 +102,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+AUTH_USER_MODEL = 'user.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
