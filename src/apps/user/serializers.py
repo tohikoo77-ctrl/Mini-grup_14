fa68f379bd60d494d4de50905d788ac2db0c0260
iskandar7171 <tolagankhojaeviskandar@gmail.com>
2026-04-28 21:05:16 +0300

    add: user auth and validation

--- src/apps/user/serializers.py
+++ src/apps/user/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import User, Seller, SellerWallet, Client, Cart, Favorite, Tag, LeadStatus
+from django.contrib.auth import get_user_model,authenticate
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -48,3 +49,41 @@ class LeadStatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = LeadStatus
         fields = "__all__"
+    
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "password", "phone_number", "role"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def validate_phone_number(self, value):
+        if not value.startswith("+"):
+            raise serializers.ValidationError("Phone must start with +")
+        return value
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+    
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if not user:
+            raise serializers.ValidationError("Wrong username or password")
+        return user
+    
+def create(self, validated_data):
+    user = User.objects.create_user(**validated_data)
+
+    if user.role == "user":
+        Client.objects.create(user=user)
+    elif user.role == "seller":
+        Seller.objects.create(user=user, name=user.username)
+
+    return user
--- src/apps/user/views.py
+++ src/apps/user/views.py
@@ -2,7 +2,30 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import *
 from .serializers import *
+from rest_framework.views import APIView
+from .permissions import IsSeller
+from rest_framework.response import Response
+from rest_framework import generics
+from django.contrib.auth import authenticate
+from rest_framework import status
 
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        user = authenticate(
+            username=request.data["username"],
+            password=request.data["password"]
+        )
+
+        if user:
+            return Response({"message": "Login successful"})
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
@@ -42,3 +65,10 @@ class TagViewSet(ModelViewSet):
 class LeadStatusViewSet(ModelViewSet):
     queryset = LeadStatus.objects.all()
     serializer_class = LeadStatusSerializer
+
+
+class SellerView(APIView):
+    permission_classes = [IsSeller]
+
+    def get(self, request):
+        return Response({"message": "Only sellers can see this"})
