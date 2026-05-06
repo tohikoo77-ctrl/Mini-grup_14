45a6e5f774f2d7036cdac2d400ce61ae8bb801ad
Izzatillo <izzatillaasatullayev55@gmail.com>
2026-05-06 16:45:02 +0500

    add : env

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,11 +1,16 @@
 import os
 from pathlib import Path
+from environ import Env
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-s7x30#imooj@7q3(3$+%4qs8bp)9ntw)46&g-qnklwc@77+vi")
-DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1").split()
+# Load environment variables
+env = Env()
+env.read_env(os.path.join(BASE_DIR.parent, '.env'))
+
+SECRET_KEY = env("DJANGO_SECRET_KEY", default="django-insecure-s7x30#imooj@7q3(3$+%4qs8bp)9ntw)46&g-qnklwc@77+vi")
+DEBUG = env.bool("DJANGO_DEBUG", default=True)
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default="localhost 127.0.0.1".split())
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -51,12 +56,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql" if os.getenv("DB_NAME") else "django.db.backends.sqlite3",
-        "NAME": os.getenv("DB_NAME") or BASE_DIR / "db.sqlite3",
-        "USER": os.getenv("DB_USER", ""),
-        "PASSWORD": os.getenv("DB_PASSWORD", ""),
-        "HOST": os.getenv("DB_HOST", ""),
-        "PORT": os.getenv("DB_PORT", ""),
+        "ENGINE": "django.db.backends.postgresql" if env("DB_NAME", default="") else "django.db.backends.sqlite3",
+        "NAME": env("DB_NAME", default=str(BASE_DIR / "db.sqlite3")),
+        "USER": env("DB_USER", default=""),
+        "PASSWORD": env("DB_PASSWORD", default=""),
+        "HOST": env("DB_HOST", default=""),
+        "PORT": env("DB_PORT", default=""),
     }
 }
 
@@ -74,13 +79,13 @@ USE_TZ = True
 
 AUTH_USER_MODEL = "user.User"
 
-EMAIL_BACKEND = os.environ.get("DJANGO_EMAIL_BACKEND", "django.core.mail.backends.console.EmailBackend")
-EMAIL_HOST = os.environ.get("DJANGO_EMAIL_HOST", "smtp.gmail.com")
-EMAIL_PORT = int(os.environ.get("DJANGO_EMAIL_PORT", 587))
-EMAIL_USE_TLS = os.environ.get("DJANGO_EMAIL_USE_TLS", "True") == "True"
-EMAIL_HOST_USER = os.environ.get("DJANGO_EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.environ.get("DJANGO_EMAIL_HOST_PASSWORD", "")
-DEFAULT_FROM_EMAIL = os.environ.get("DJANGO_DEFAULT_FROM_EMAIL", "webmaster@localhost")
+EMAIL_BACKEND = env("DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend")
+EMAIL_HOST = env("DJANGO_EMAIL_HOST", default="smtp.gmail.com")
+EMAIL_PORT = env.int("DJANGO_EMAIL_PORT", default=587)
+EMAIL_USE_TLS = env.bool("DJANGO_EMAIL_USE_TLS", default=True)
+EMAIL_HOST_USER = env("DJANGO_EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env("DJANGO_EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = env("DJANGO_DEFAULT_FROM_EMAIL", default="webmaster@localhost")
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
